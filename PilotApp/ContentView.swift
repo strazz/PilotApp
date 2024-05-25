bd17920cbe5e2bd1f4ab5f5f8a8ed63aece7d97a
Giovanni Romaniello <giovanni.romaniello@vodafone.com>
2024-05-25 11:01:31 +0200

    Initial Commit

---
+++ PilotApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PilotApp
+//
+//  Created by Giovanni Romaniello on 25/05/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PilotApp/PilotAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PilotAppApp.swift
+//  PilotApp
+//
+//  Created by Giovanni Romaniello on 25/05/24.
+//
+
+import SwiftUI
+
+@main
+struct PilotAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
